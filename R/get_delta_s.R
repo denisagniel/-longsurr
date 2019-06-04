db4206f62641168d265a7600767d58163bb4e446
Agniel, Denis <dagniel@rand.org>
2019-06-03 20:25:54 -0500

    Add kernel-squared estimators.

--- R/get_delta_s.R
+++ R/get_delta_s.R
@@ -50,9 +50,25 @@ get_delta_s <- function(y_t = NULL, y_c = NULL, X_t = NULL, X_c = NULL) {
   sspline_yhat = predict(sspline_fit, newdata = list(X_t = X_c), type = 'response')
   
   k_sspline_fit <- R.s.estimate(sone = predict(sspline_fit, type ='response'),
-                                szero = fgam_yhat,
+                                szero = sspline_yhat,
                                 yone = y_t,
                                 yzero = y_c, var = FALSE)
+  k_pca1_fit <- R.s.estimate(sone = predict(pca1_fit, fdX_t),
+                                szero = pca1_yhat,
+                                yone = y_t,
+                                yzero = y_c, var = FALSE)
+  k_pca2_fit <- R.s.estimate(sone = predict(pca2_fit, fdX_t),
+                             szero = pca2_yhat,
+                             yone = y_t,
+                             yzero = y_c, var = FALSE)
+  k_pca3_fit <- R.s.estimate(sone = predict(pca3_fit, fdX_t),
+                             szero = pca3_yhat,
+                             yone = y_t,
+                             yzero = y_c, var = FALSE)
+  k_k_fit <- R.s.estimate(sone = predict(k_fit, fdX_t),
+                             szero = k_yhat,
+                             yone = y_t,
+                             yzero = y_c, var = FALSE)
   
   
   delta_ss <-
@@ -60,6 +76,9 @@ get_delta_s <- function(y_t = NULL, y_c = NULL, X_t = NULL, X_c = NULL) {
            pca1 = mean(pca1_yhat) - mean(y_c),
            pca2 = mean(pca2_yhat) - mean(y_c),
            pca3 = mean(pca3_yhat) - mean(y_c),
+           kpca1 = k_pca1_fit$delta.s,
+           kpca2 = k_pca2_fit$delta.s,
+           kpca3 = k_pca3_fit$delta.s,
               fgam = mean(fgam_yhat) - mean(y_c),
            kfgam = k_fgam_fit$delta.s,
            sspline = mean(sspline_yhat) - mean(y_c),
